98274645f6ee61d1c45086a5c3ed4b555dea31d9
Artem Borovik <polzovatellllk@gmail.com>
2024-06-01 00:56:25 +0300

    minor fixes

--- docker/cfggen.py
+++ docker/cfggen.py
@@ -81,98 +81,98 @@ with open("../../_build/config/env.sty", "w") as file:
     # config['department']['']
     recendentMoreInfo = "\n    ".join(config["recendent"]["moreInfo"])
     file.write(
-        f"""\def \envDiplomMinistr {{{config['ministry']}}}
-\def \envDiplomEducation {{{config['education']}}}
-\def \envDiplomUniversity {{{config['university']}}}
-\def \envDiplomCathedra {{{config['cathedra']}}}
+        f"""\\def \\envDiplomMinistr {{{config['ministry']}}}
+\\def \\envDiplomEducation {{{config['education']}}}
+\\def \\envDiplomUniversity {{{config['university']}}}
+\\def \\envDiplomCathedra {{{config['cathedra']}}}
 
-\def \envDiplomTitle {{{config['title']}}}
-\def \envDiplomTitleUppercased {{{config['title'].upper()}}}
-\def \envDiplomEnterprice {{{config['enterprise']}}}
-\def \envDiplomNumber {{{config['number']}}}
-\def \envDiplomVersion {{{config['version']}}}
+\\def \\envDiplomTitle {{{config['title']}}}
+\\def \\envDiplomTitleUppercased {{{config['title'].upper()}}}
+\\def \\envDiplomEnterprice {{{config['enterprise']}}}
+\\def \\envDiplomNumber {{{config['number']}}}
+\\def \\envDiplomVersion {{{config['version']}}}
 
-\def \envDiplomCity {{{config['city']}}}
+\\def \\envDiplomCity {{{config['city']}}}
 
-\def \envDiplomStudentGroupName {{{config['student']['groupName']}}}
-\def \envDiplomStudentGroupNumber {{{config['student']['groupNumber']}}}
-\def \envDiplomStudentCard {{{config['student']['card']}}}
+\\def \\envDiplomStudentGroupName {{{config['student']['groupName']}}}
+\\def \\envDiplomStudentGroupNumber {{{config['student']['groupNumber']}}}
+\\def \\envDiplomStudentCard {{{config['student']['card']}}}
 
-\def \envDiplomStudentSurname {{{config['student']['surname']}}}
-\def \envDiplomStudentInitials {{{config['student']['initials']}}}
-\def \envDiplomStudentInfo{{{config['student']['info']}}}
+\\def \\envDiplomStudentSurname {{{config['student']['surname']}}}
+\\def \\envDiplomStudentInitials {{{config['student']['initials']}}}
+\\def \\envDiplomStudentInfo{{{config['student']['info']}}}
 
 % Руководитель
-\def \envDiplomTeacherSurname {{{config['teacher']['surname']}}}
-\def \envDiplomTeacherInitials {{{config['teacher']['initials']}}}
-\def \envDiplomTeacherInfo{{{config['teacher']['info']}}}
+\\def \\envDiplomTeacherSurname {{{config['teacher']['surname']}}}
+\\def \\envDiplomTeacherInitials {{{config['teacher']['initials']}}}
+\\def \\envDiplomTeacherInfo{{{config['teacher']['info']}}}
 
 % Консультант по экономическому разделу
-\def \envDiplomEconomicSurname {{{config['economic']['surname']}}}
-\def \envDiplomEconomicInitials {{{config['economic']['initials']}}}
-\def \envDiplomEconomicInfo{{{config['economic']['info']}}}
+\\def \\envDiplomEconomicSurname {{{config['economic']['surname']}}}
+\\def \\envDiplomEconomicInitials {{{config['economic']['initials']}}}
+\\def \\envDiplomEconomicInfo{{{config['economic']['info']}}}
 
 % Консультант по ЕСПД
-\def \envDiplomEspdSurname {{{config['espd']['surname']}}}
-\def \envDiplomEspdInitials {{{config['espd']['initials']}}}
-\def \envDiplomEspdInfo{{{config['espd']['info']}}}
+\\def \\envDiplomEspdSurname {{{config['espd']['surname']}}}
+\\def \\envDiplomEspdInitials {{{config['espd']['initials']}}}
+\\def \\envDiplomEspdInfo{{{config['espd']['info']}}}
 
 % Рецензент
-\def \envDiplomRecendentSurname {{{config['recendent']['surname']}}}
-\def \envDiplomRecendentInitials {{{config['recendent']['initials']}}}
-\def \envDiplomRecendentInfo{{{config['recendent']['info']}}}
-\def \envDiplomRecendentMoreInfo{{\n    {recendentMoreInfo}\n}}
+\\def \\envDiplomRecendentSurname {{{config['recendent']['surname']}}}
+\\def \\envDiplomRecendentInitials {{{config['recendent']['initials']}}}
+\\def \\envDiplomRecendentInfo{{{config['recendent']['info']}}}
+\\def \\envDiplomRecendentMoreInfo{{\n    {recendentMoreInfo}\n}}
 
-\def \envDiplomHeadCathedraSurname{{{config['department']['surname']}}}
-\def \envDiplomHeadDepartmentInitials{{{config['department']['initials']}}}
-\def \envDiplomHeadDepartmentInfo {{{config['department']['info']}}}
+\\def \\envDiplomHeadCathedraSurname{{{config['department']['surname']}}}
+\\def \\envDiplomHeadDepartmentInitials{{{config['department']['initials']}}}
+\\def \\envDiplomHeadDepartmentInfo {{{config['department']['info']}}}
 
 % Экономический раздел
-\def \envGPREstimateLOC {{{estimate_loc}}}
-\def \envGPRActualLOC {{{actual_loc}}}
-\def \envGPRDevTimeMonth {{{config["economic_data"]["dev_time_months"]}}}
-\def \envGPRDevTimeMonthReadable {{{dev_time_readable}}}
-\def \envGPRDevTimeDays {{{dev_time_days}}}
-\def \envGPRWorkingDaysInMonth{{{working_days_month}}}
-\def \envGPRBaseRate {{{config["economic_data"]["base_rate"]}}}
-\def \envGPRDevCategory {{{config["economic_data"]["dev_category"]}}}
-\def \envGPRDevCategoryMultiplier {{{config["economic_data"]["dev_category_multiplier"]}}}
-\def \envGPRPremiumCoefficient {{{config["economic_data"]["premium_coefficient"]}}}
-\def \envGPRValueZPosn {{{value_ZP_osn}}}
-\def \envGPRExtraPaymentPercents {{{config["economic_data"]["extra_payment_percents"]}}}
-\def \envGPRValueZPdop {{{value_ZP_dop}}}
-\def \envGPRValueZP {{{value_ZP}}}
-\def \envGPRValueRsoc {{{value_Rsoc}}}
-\def \envGPRMaterialPercents {{{config["economic_data"]["material_percents"]}}}
-\def \envGPRValueRm {{{value_R_m}}}
-\def \envGPRMachineHourCost {{{config["economic_data"]["machine_hour_cost"]}}}
-\def \envGPRDebugPortion {{{config["economic_data"]["debug_portion"]}}}
-\def \envGPRValueRmv {{{value_R_mv}}}
-\def \envGPROtherSpendsPercents {{{config["economic_data"]["other_spends_percents"]}}}
-\def \envGPRValueRnr {{{value_R_nr}}}
-\def \envGPRValueSR {{{value_SR}}}
-\def \envGPRUsageSpendsPercents {{{config["economic_data"]["usage_spends_percents"]}}}
-\def \envGPRValueRo {{{value_Ro}}}
-\def \envGPRSupportSpendsPercents {{{config["economic_data"]["support_spends_percents"]}}}
-\def \envGPRValueRso {{{value_Rso}}}
-\def \envGPRValueSP {{{value_SP}}}
-\def \envGPRProfitabilityPercents {{{config["economic_data"]["profitability_percents"]}}}
-\def \envGPRValueP {{{value_P}}}
-\def \envGPRValueCp {{{value_Cp}}}
-\def \envGPRTaxPercents {{{config["economic_data"]["tax_percents"]}}}
-\def \envGPRValueNDSpp {{{value_NDS_pp}}}
-\def \envGPRValueCo {{{value_Co}}}
-\def \envGPRIncomeTaxPercents {{{config["economic_data"]["income_tax_percents"]}}}
-\def \envGPRValuePC {{{value_PC}}}
+\\def \\envGPREstimateLOC {{{estimate_loc}}}
+\\def \\envGPRActualLOC {{{actual_loc}}}
+\\def \\envGPRDevTimeMonth {{{config["economic_data"]["dev_time_months"]}}}
+\\def \\envGPRDevTimeMonthReadable {{{dev_time_readable}}}
+\\def \\envGPRDevTimeDays {{{dev_time_days}}}
+\\def \\envGPRWorkingDaysInMonth{{{working_days_month}}}
+\\def \\envGPRBaseRate {{{config["economic_data"]["base_rate"]}}}
+\\def \\envGPRDevCategory {{{config["economic_data"]["dev_category"]}}}
+\\def \\envGPRDevCategoryMultiplier {{{config["economic_data"]["dev_category_multiplier"]}}}
+\\def \\envGPRPremiumCoefficient {{{config["economic_data"]["premium_coefficient"]}}}
+\\def \\envGPRValueZPosn {{{value_ZP_osn}}}
+\\def \\envGPRExtraPaymentPercents {{{config["economic_data"]["extra_payment_percents"]}}}
+\\def \\envGPRValueZPdop {{{value_ZP_dop}}}
+\\def \\envGPRValueZP {{{value_ZP}}}
+\\def \\envGPRValueRsoc {{{value_Rsoc}}}
+\\def \\envGPRMaterialPercents {{{config["economic_data"]["material_percents"]}}}
+\\def \\envGPRValueRm {{{value_R_m}}}
+\\def \\envGPRMachineHourCost {{{config["economic_data"]["machine_hour_cost"]}}}
+\\def \\envGPRDebugPortion {{{config["economic_data"]["debug_portion"]}}}
+\\def \\envGPRValueRmv {{{value_R_mv}}}
+\\def \\envGPROtherSpendsPercents {{{config["economic_data"]["other_spends_percents"]}}}
+\\def \\envGPRValueRnr {{{value_R_nr}}}
+\\def \\envGPRValueSR {{{value_SR}}}
+\\def \\envGPRUsageSpendsPercents {{{config["economic_data"]["usage_spends_percents"]}}}
+\\def \\envGPRValueRo {{{value_Ro}}}
+\\def \\envGPRSupportSpendsPercents {{{config["economic_data"]["support_spends_percents"]}}}
+\\def \\envGPRValueRso {{{value_Rso}}}
+\\def \\envGPRValueSP {{{value_SP}}}
+\\def \\envGPRProfitabilityPercents {{{config["economic_data"]["profitability_percents"]}}}
+\\def \\envGPRValueP {{{value_P}}}
+\\def \\envGPRValueCp {{{value_Cp}}}
+\\def \\envGPRTaxPercents {{{config["economic_data"]["tax_percents"]}}}
+\\def \\envGPRValueNDSpp {{{value_NDS_pp}}}
+\\def \\envGPRValueCo {{{value_Co}}}
+\\def \\envGPRIncomeTaxPercents {{{config["economic_data"]["income_tax_percents"]}}}
+\\def \\envGPRValuePC {{{value_PC}}}
 
 
 
 
 
 % Прочее
-\def \envDiplomDateInput{{<<\\underline{{\hspace{{0.5cm}}}}>> \\underline{{\hspace{{2cm}}}} \ESKDtheYear~г.}}
-\def \\No {{\\textnumero}}
-\date {{{config["year"]}/месяц/день}}
+\\def \\envDiplomDateInput{{<<\\underline{{\\hspace{{0.5cm}}}}>> \\underline{{\\hspace{{2cm}}}} \\ESKDtheYear~г.}}
+\\def \\No {{\\textnumero}}
+\\date {{{config["year"]}/месяц/день}}
 """
     )
 
--- docker/support_builder.Dockerfile
+++ docker/support_builder.Dockerfile
@@ -1,5 +1,4 @@
 FROM ubuntu
 
 RUN apt-get update
-RUN apt-get install -y librsvg2-bin
-RUN apt-get install -y make python3
+RUN apt-get install -y librsvg2-bin make python3
