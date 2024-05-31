3960760eecaaa28a07d7bb698972b1bc01f8584a
Artem Borovik <polzovatellllk@gmail.com>
2024-05-31 14:27:59 +0300

    build performance

--- docker/_build.sh
+++ docker/_build.sh
@@ -1,8 +1,12 @@
 cd $(dirname $0)/../..
 
-set -ex
-
 # export PATH="$(cd /usr/local/texlive/????/bin/*; pwd):$PATH"
 
-rm -f _build/*.pdf
 latexmk -pdf -halt-on-error -output-directory=_build -interaction=nonstopmode
+ret_code=$?
+
+if test $ret_code -ne 0
+then
+    # rm _build/*.pdf
+    exit $ret_code
+fi
--- docker/_support_build.sh
+++ docker/_support_build.sh
@@ -1,5 +1,11 @@
 cd $(dirname $0)/../..
 
-set -ex
-
 make --directory=graduation_project_report_common/docker
+ret_code=$?
+
+if test $ret_code -ne 0
+then
+    rm _build/*.pdf
+    rm -r _build/config
+    exit $ret_code
+fi
