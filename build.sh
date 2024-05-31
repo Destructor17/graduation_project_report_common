b81400dc4a9d21abc4d92f8145be4cfbd1706e45
Artem Borovik <polzovatellllk@gmail.com>
2024-05-31 16:21:39 +0300

    rm unused

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ set -ex
 
 docker-compose run --rm support_builder sh /build/graduation_project_report_common/docker/_support_build.sh 
 docker-compose run --rm builder sh /build/graduation_project_report_common/docker/_build.sh
- 
