16f4cd10eb57094cfb26dfd40f3c2ad0d42a9d5c
Artem Borovik <polzovatellllk@gmail.com>
2024-06-04 22:12:39 +0300

    gprctl

--- scripts/gprctl.py
+++ scripts/gprctl.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import os
 import subprocess
+import shutil
+
+from_stdin = "__file__" not in globals() or __file__ == "<stdin>"
 
 
 def update_submodule():
@@ -22,7 +25,11 @@ def check_has_gpr_common_submodule():
     return len(lines) == 1
 
 
-root_dir = os.path.dirname(os.path.realpath(__file__))
+root_dir = (
+    os.path.realpath(os.curdir)
+    if from_stdin
+    else os.path.dirname(os.path.realpath(__file__))
+)
 os.chdir(root_dir)
 if not os.path.exists(
     os.path.join(root_dir, "graduation_project_report_common", "scripts", "run.py")
@@ -43,6 +50,17 @@ if not os.path.exists(
     )
     if ret_code != 0:
         exit(ret_code)
+
+if from_stdin:
+    print(root_dir)
+    shutil.copy(
+        os.path.join(
+            root_dir, "graduation_project_report_common", "scripts", "gprctl.py"
+        ),
+        os.path.join(root_dir, "gprctl.py"),
+    )
+else:
+    print(__file__)
 
 import graduation_project_report_common.scripts.run as run
 
