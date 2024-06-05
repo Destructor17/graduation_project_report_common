e7f4f001daebbc3a025a5e5d8c2b5a48ef8a477e
Artem Borovik <polzovatellllk@gmail.com>
2024-06-05 19:20:14 +0300

    unused line fix

--- watch.sh
+++ watch.sh
@@ -6,6 +6,5 @@ while true
 do
     clear
     docker-compose run --rm builder sh /build/graduation_project_report_common/docker/_build.sh
-    sh ../../a.sh
     sh _list_watched.sh | inotifywait -e modify,create,delete,move --fromfile -
 done
